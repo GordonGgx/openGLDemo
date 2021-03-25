ee3b24b2875b28d0199f1a02b411ab7a4bd80287
ggx <abcgg8@qq.com>
2021-03-25 23:24:25 +0800

    学习纹理

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <cmath>
 #include "shader.h"
+#include "stb/stb_image.h"
 
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
@@ -58,10 +59,10 @@ int main(int argc, char *argv[])
 	Shader shader("shader/vertex.vs", "shader/fragment.fs");
 
 	const float firstTriangle[] = {
-		// first triangle        //颜色
-		-0.9f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // left
-		-0.0f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // right
-		-0.45f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // top
+		//-----位置-----     //颜色            //纹理
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // 左下
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // 右下
+		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.5f, 1.0f	  // 上
 
 	};
 	// const float secondTriangle[] = {
@@ -94,13 +95,32 @@ int main(int argc, char *argv[])
 	//颜色属性(layout location=1)
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-
+	//纹理属性
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 	//第二个三角形初始化
 	// glBindVertexArray(vao[1]);
 	// glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
 	// glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
 	// glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	// glEnableVertexAttribArray(0);
+
+	//纹理
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// 为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// 加载并生成纹理
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char *data = stbi_load("images/wall.jpg", &width, &height, &nrChannels, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(data);
 
 	//开启线框模式
 	// glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
@@ -110,9 +130,12 @@ int main(int argc, char *argv[])
 		/* render here */
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		// bind Texture
+		glBindTexture(GL_TEXTURE_2D, texture);
 		shader.use();
 		glBindVertexArray(vao[0]);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		// float timeValue = glfwGetTime();
 		// float green = (sin(timeValue) / 2.0f) + 0.5f;
